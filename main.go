0fc0c9384e4deb5e3c5d86864aaa8209e8950707
erietz <ewrietz@gmail.com>
2023-05-19 20:03:20 -0500

    add html template

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"os"
+	"time"
+
 	"github.com/erietz/health/src"
 )
 
+type EmailData struct {
+	PageTitle  string
+	LoadAvg    health.LoadAvg
+	Processors int
+}
+
 func main() {
-	numProcessors := health.GetProcessors()
-	loadAverage := health.GetLoadAvg()
-	fmt.Println(numProcessors)
-	fmt.Println(loadAverage)
+	tmpl := template.Must(template.ParseFiles("views/email.gohtml"))
+	data := EmailData{
+		PageTitle:  "System information as of " + time.Now().Format("2006-01-02 15:04:05"),
+		LoadAvg:    health.GetLoadAvg(),
+		Processors: health.GetProcessors(),
+	}
+
+	if err := tmpl.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+
 }
